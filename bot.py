e6d5621b10b9b4551a3d065596e7153239fac0a1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-04 22:21:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,30 @@
-import logging
+from pyrogram import Client as Clinton
+from config import *
 import os
-from aiohttp import web
-from pyrogram import Client
-from config import Config
-from plugins import web_server
 
-# Set up logging configurations
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
-PORT = 8080
-
-# Create download directory if it doesn't exist
-if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-    os.makedirs(Config.DOWNLOAD_LOCATION)
-
-class Bot(Client):
+class Bot(Clinton):
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
 
     def __init__(self):
         super().__init__(
-            session_name=Config.SESSION_NAME,
-            bot_token=Config.BOT_TOKEN,
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            workers=200,
-            plugins={"root": "plugins"},
+            session_name=SESSION_NAME,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "main"},
             sleep_threshold=10,
         )
-
     async def start(self):
-        app = web.Application()
-        app.add_routes([web.get("/", web_server)])
-        runner = web.AppRunner(app)
-        await runner.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(runner, bind_address, PORT).start()
-
+        await super().start()
+        me = await self.get_me()      
+        print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+       
     async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot stopped. Bye.")
+       await super().stop()      
+       print("Bot Restarting........")
 
-# Create and run the bot instance
-app = Bot()
-app.run()
+
+bot = Bot()
+bot.run()
