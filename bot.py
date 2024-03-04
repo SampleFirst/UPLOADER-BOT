130cfeb3d05abe1a99dfe9a42742ef393abdf88c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-04 21:07:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
+            session_name=Config.SESSION_NAME,
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
